f0d24039f6aecef8d504aca9f7f5b0bf5099c2bd
Robin Tram <robin.tram@gmail.com>
2018-10-18 13:47:24 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,8 @@ end
 def counts_number_of_bears_with_goofy_temperaments
   "SELECT COUNT(temperament)
     FROM bears
-    GROUP BY temperament;    
+    GROUP BY temperament
+    WHERE temperament = ;    
   "
 end
 
