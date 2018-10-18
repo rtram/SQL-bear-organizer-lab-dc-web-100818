9c0e09322c728036001808a85bb14db8589e4ad8
Robin Tram <robin.tram@gmail.com>
2018-10-18 13:45:03 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "black", "feisty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "M", "brown", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9, "F", "polar", "festive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown ", 11, "M", "brown", "feelin' himself", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 20, "F", "panda", "hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 50, "M", "armored", "spiteful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "F", "black", "scared", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null , 11, "M", "brown", "full", 1);
